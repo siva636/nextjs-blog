cc3d6cdc0661186c41650686cba04dfb03c684aa
siva <siva636@gmail.com>
2024-02-25 05:31:17 +0530

    Minor UI/UX improvements

--- app/utils/style-utils.ts
+++ app/utils/style-utils.ts
@@ -1,7 +1,7 @@
 export function primaryActionClasses() {
-  return 'border border-sky-500 text-white bg-sky-500 hover:bg-sky-600 py-1.5 px-3 rounded-full';
+  return 'w-32 text-center border border-sky-500 text-white bg-sky-500 hover:bg-sky-600 py-1.5 px-3 rounded-full';
 }
 
 export function secondaryActionClasses() {
-  return 'border border-sky-500 hover:text-white hover:hover:bg-sky-600 py-1.5 px-3 rounded-full';
+  return 'w-32 text-center border border-sky-500 hover:text-white hover:hover:bg-sky-600 py-1.5 px-3 rounded-full';
 }
