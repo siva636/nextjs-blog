203031d865730132aa3cabcdacbe4a547974abbc
siva <siva636@gmail.com>
2024-04-21 05:59:13 +0530

    Custom error page

---
+++ app/error.tsx
@@ -0,0 +1,8 @@
+'use client';
+export default function ErrorPage() {
+  return (
+    <div className='flex h-screen'>
+      <div className='m-auto text-red-500'>Oops! Something went wrong!</div>
+    </div>
+  );
+}
