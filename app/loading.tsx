b64f124ba7b5d2d6fa4f8dcce0c41f65855330ca
siva <siva636@gmail.com>
2024-04-24 06:26:18 +0530

    Minor code clean up

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,13 +1,21 @@
 import PostCardLoadingSkeleton from '@/components/post-card-loading-skeleton';
 
 export default function Loading() {
+  // return (
+  //   <div className='flex flex-wrap justify-start gap-2'>
+  //     <PostCardLoadingSkeleton />
+  //     <PostCardLoadingSkeleton />
+  //     <PostCardLoadingSkeleton />
+  //     <PostCardLoadingSkeleton />
+  //     <PostCardLoadingSkeleton />
+  //   </div>
+  // );
+
   return (
     <div className='flex flex-wrap justify-start gap-2'>
-      <PostCardLoadingSkeleton />
-      <PostCardLoadingSkeleton />
-      <PostCardLoadingSkeleton />
-      <PostCardLoadingSkeleton />
-      <PostCardLoadingSkeleton />
+      {Array(5)
+        .fill(<PostCardLoadingSkeleton />)
+        .map((x) => x)}
     </div>
   );
 }
