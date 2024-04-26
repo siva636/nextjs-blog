9a3cadac04f5f5bec8625d0bc86cc4eb41a0543e
siva <siva636@gmail.com>
2024-04-26 10:40:49 +0530

    Minor fix in new post loading skeleton

--- app/create/loading.tsx
+++ app/create/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <>
+    <div className='prose'>
       <h1 className='w-[300px] h-10 mb-4 rounded bg-gray-200'></h1>
       <div className='w-full h-10 mb-4 rounded bg-gray-200'></div>
       <div className='w-full h-52 mb-4 rounded bg-gray-200'></div>
@@ -9,6 +9,6 @@ export default function Loading() {
         <div className='w-28 h-10 mr-2 rounded bg-gray-200'></div>
         <div className='w-28 h-10 mr-2 rounded bg-gray-200'></div>
       </div>
-    </>
+    </div>
   );
 }
