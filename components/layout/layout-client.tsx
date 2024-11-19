de39a5bbcd2538a356c6c69bbc70b444f056b5b5
siva <siva636@gmail.com>
2024-11-19 16:41:11 +0530

    Light theme made as default

--- components/layout/layout-client.tsx
+++ components/layout/layout-client.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute='class'
-          defaultTheme='dark'
+          defaultTheme='light'
           enableSystem
           disableTransitionOnChange
         >
