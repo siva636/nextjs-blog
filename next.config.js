d31040372ed024305df3826dc241adab92c27051
siva <siva636@gmail.com>
2024-11-21 16:33:20 +0530

    Home page to feed redirect configured in next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/feed/1',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
